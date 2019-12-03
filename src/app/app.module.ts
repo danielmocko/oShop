a4339be870de5f8390b3300b1e9f0994078d6c10
Daniel Mocko <danielmocko50@gmail.com>
2019-12-03 17:34:40 +0100

    Sredjen nav-bar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,25 @@
-import { environment } from './../environments/environment';
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { AngularFireModule } from '@angular/fire';
-import { AngularFireAuthModule } from '@angular/fire/auth';
-import { AngularFireDatabaseModule } from '@angular/fire/database';
-import { RouterModule} from "@angular/router"
+import { environment } from "./../environments/environment";
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { AngularFireModule } from "@angular/fire";
+import { AngularFireAuthModule } from "@angular/fire/auth";
+import { AngularFireDatabaseModule } from "@angular/fire/database";
+import { RouterModule } from "@angular/router";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { from } from 'rxjs';
-import { HomeComponent } from './components/home/home.component';
-import { ProductsComponent } from './components/products/products.component';
-import { ShoppingCartComponent } from './components/shopping-cart/shopping-cart.component';
-import { CheckOutComponent } from './components/check-out/check-out.component';
-import { OrderSuccessComponent } from './components/order-success/order-success.component';
-import { MyOrdersComponent } from './components/my-orders/my-orders.component';
-import { AdminProductsComponent } from './components/admin/admin-products/admin-products.component';
-import { AdminOrdersComponent } from './components/admin/admin-orders/admin-orders.component';
-import { LoginComponent } from './components/login/login.component';
-import { BsNavbarComponent } from './components/bs-navbar/bs-navbar.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { from } from "rxjs";
+import { HomeComponent } from "./components/home/home.component";
+import { ProductsComponent } from "./components/products/products.component";
+import { ShoppingCartComponent } from "./components/shopping-cart/shopping-cart.component";
+import { CheckOutComponent } from "./components/check-out/check-out.component";
+import { OrderSuccessComponent } from "./components/order-success/order-success.component";
+import { MyOrdersComponent } from "./components/my-orders/my-orders.component";
+import { AdminProductsComponent } from "./components/admin/admin-products/admin-products.component";
+import { AdminOrdersComponent } from "./components/admin/admin-orders/admin-orders.component";
+import { LoginComponent } from "./components/login/login.component";
+import { BsNavbarComponent } from "./components/bs-navbar/bs-navbar.component";
+import { NgbModule } from "@ng-bootstrap/ng-bootstrap";
 
 @NgModule({
   declarations: [
@@ -39,21 +40,23 @@ import { BsNavbarComponent } from './components/bs-navbar/bs-navbar.component';
     AppRoutingModule,
     AngularFireModule.initializeApp(environment.firebase),
     AngularFireAuthModule,
+    NgbModule,
     AngularFireDatabaseModule,
 
     RouterModule.forRoot([
-      { path: '', component: HomeComponent },
-      { path: 'products', component: ProductsComponent },
-      { path: 'shopping-cart', component: ShoppingCartComponent },
-      { path: 'check-out', component: CheckOutComponent },
-      { path: 'order-success', component: OrderSuccessComponent },
-      { path: 'login', component: LoginComponent },
-      { path: 'admin/products', component:AdminProductsComponent},
-      { path: 'admin/orders', component:AdminOrdersComponent}
-
+      { path: "home", component: HomeComponent },
+      { path: "products", component: ProductsComponent },
+      { path: "shopping-cart", component: ShoppingCartComponent },
+      { path: "check-out", component: CheckOutComponent },
+      { path: "order-success", component: OrderSuccessComponent },
+      { path: "login", component: LoginComponent },
+      { path: "admin/products", component: AdminProductsComponent },
+      { path: "admin/orders", component: AdminOrdersComponent },
+      { path: "my/orders", component: MyOrdersComponent },
+      { path: "**", component: HomeComponent }
     ])
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
