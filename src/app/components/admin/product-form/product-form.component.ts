915579968b9b1eb9ef878b9b900763ba4dcdda38
Daniel Mocko <danielmocko50@gmail.com>
2019-12-05 15:02:48 +0100

    Firebase Get works

--- src/app/components/admin/product-form/product-form.component.ts
+++ src/app/components/admin/product-form/product-form.component.ts
@@ -3,9 +3,9 @@ import { AngularFireList } from "@angular/fire/database";
 import { ProductService } from "./../../../services/product.service";
 import { CategoryService } from "./../../../services/category.service";
 import { Component, OnInit, OnDestroy } from "@angular/core";
-import { take } from 'rxjs/operators';
+import { take } from "rxjs/operators";
 import { map } from "rxjs/operators";
-import { Router, ActivatedRoute } from '@angular/router';
+import { Router, ActivatedRoute } from "@angular/router";
 
 @Component({
   selector: "app-product-form",
@@ -14,19 +14,34 @@ import { Router, ActivatedRoute } from '@angular/router';
 })
 export class ProductFormComponent implements OnInit {
   categories$;
-  product={};
+  product = {};
 
   constructor(
-    private route:ActivatedRoute,
-    private router:Router,
+    private route: ActivatedRoute,
+    private router: Router,
     private categoryService: CategoryService,
     private productService: ProductService
   ) {
+    //this.categories$ = this.categoryService.getCategories().valueChanges();
+    this.categories$ = this.categoryService
+      .getCategories()
+      .snapshotChanges()
+      .pipe(
+        map(categories =>
+          categories.map(a => {
+            const key = a.key;
+            const name = a.payload.child("name").val();
+            console.log({ key, name });
+            return { key, name };
+          })
+        )
+      );
 
-    this.categories$ = this.categoryService.getCategories().valueChanges();
+    /*
     let id=this.route.snapshot.paramMap.get('id');
     console.log(id);
     if(id) this.productService.getProduct(id).snapshotChanges().pipe(take(1)).subscribe(p=>this.product=p);
+    */
   }
 
   ngOnInit() {}
@@ -34,8 +49,12 @@ export class ProductFormComponent implements OnInit {
   save(product) {
     console.log(product);
     this.productService.create(product);
-    this.router.navigate(['/admin/products']);
+    this.router.navigate(["/admin/products"]);
   }
 
-
+  logCat() {
+    this.categories$.forEach(category => {
+      console.log(category);
+    });
+  }
 }
